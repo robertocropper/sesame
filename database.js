abd33fc314e093502bd518b48abd461808fe5e20
Roberto <robertocropper@icloud.com>
2022-03-02 16:37:40 +0000

    second commit

--- database.js
+++ database.js
@@ -10,6 +10,8 @@ const development = {
 };
 
 const production = {
+  url: process.env.DATABASE_URL,
+  /*
   user: process.env.USERNAME,
   host: process.env.HOSTNAME,
   database: process.env.DATABASE,
@@ -18,7 +20,7 @@ const production = {
   ssl: {
     rejectUnauthorized: true,
     ca: process.env.CA_CERT,
-  },
+  },*/
 };
 
 const pool = new Pool(
